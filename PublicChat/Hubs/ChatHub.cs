4dc50625c3a1328d7ad67535d8d8a76ee369806f
Ahmed Ali <prog_ahmedfcih@hotmail.com>
2020-10-04 20:47:56 +0200

    Add project files.

---
+++ PublicChat/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace PublicChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task SendMessage1(string user, string message)
+        {
+            return Clients.All.SendAsync("ReceiveOne", user, message);
+        }
+    }
+}
---
+++ PublicChat/ReqDto/MessageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PublicChat.ReqDto
+{
+    public class MessageDto
+    {
+        public string user { get; set; }
+        public string msgText { get; set; }
+    }
+}
